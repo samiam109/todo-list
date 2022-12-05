5f640556588d69f94ebdac19573c32b58b746f82
samiam109 <samueljkarp@gmail.com>
2022-12-05 09:22:33 +0000

    reduce timeout for getting todolist

--- src/components/TodoList.test.js
+++ src/components/TodoList.test.js
@@ -3,17 +3,19 @@ import userEvent from '@testing-library/user-event';
 import TodoForm from './todo/TodoForm';
 import TodoList from './todo/TodoList';
 
-test('todo input is in document', async () => {
+// ideally I'd mock the redux store instead of doing this 'integration test'
+test('todo input is in document', () => {
   renderWithRedux(<TodoForm />);
   const todo = 'Wash my car';
   const todoInput = screen.getByPlaceholderText(/enter todo/i);
   todoInput.value = todo
-
-  await userEvent.click(screen.getByText(/submit/i));
+  userEvent.click(screen.getByText(/submit/i));
 
   renderWithRedux(<TodoList />);
   setTimeout(() => {
     const todoList = screen.getAllByText(todo)
     expect(todoList.length).toBe(1)
-  })
+  }, 200)
 })
+
+//add tests for multiple list items
