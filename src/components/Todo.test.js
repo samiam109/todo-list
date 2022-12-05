89bdf7972de67de24437cde5cc846823f3eb6ceb
samiam109 <samueljkarp@gmail.com>
2022-12-05 09:20:58 +0000

    add tests for todo component

---
+++ src/components/Todo.test.js
@@ -0,0 +1,18 @@
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TodoForm from './todo/TodoForm';
+import Todo from './todo/Todo';
+
+// ideally I'd mock the redux store so that I'm not rendering every component in order to view todos
+test('todo displays correct todo value', () => {
+})
+test('todo can be completed', () => {
+})
+test('todo fades when completed', () => {
+})
+test('todo is not removed instantly after completion', () => {
+})
+test('todo has a complete button', () => {
+})
+test('todo is removed from screen 1 second after completion', () => {
+})
