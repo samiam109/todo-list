f4f20e55e7f64e8a89f56087bb718b6c7bfdec54
samiam109 <samueljkarp@gmail.com>
2022-12-05 09:51:34 +0000

    remove error context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ErrorProvider from './common/ErrorProvider';
-import ErrorNotification from './common/ErrorNotification';
 import TodoForm from './components/todo/TodoForm'
 import TodoList from './components/todo/TodoList'
 import './app.css';
@@ -9,13 +7,10 @@ function App() {
 
   return (
     <div className="app">
-      <ErrorProvider>
-        <div className='todo-content'>
-          <TodoForm />
-          <TodoList />
-        </div>
-        <ErrorNotification />
-      </ErrorProvider>
+      <div className='todo-content'>
+        <TodoForm />
+        <TodoList />
+      </div>
     </div>
   );
 }
